8d562b25a38cb89f3219e52f95b8dce0dbdb9205
andrewheartsXD <andrewheartsxd@users.noreply.github.com>
2015-02-09 23:01:31 -0800

    fixing content-type: text/plaintext to text/html makes the image display properly

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,51 +1,25 @@
-// var exec = require('child_process').exec;
-
-var querystring = require('querystring'),
-    fs = require('fs'),
-    formidable = require('formidable');
+var querystring = require('querystring'), fs = require('fs'), formidable = require('formidable');
 
 function start(response) {
   console.log('Request handler "start" was called');
 
-  var body = '<html>' +
-      '<head>' +
-      '<meta http-equiv="Content-Type" '+
-      'content="text/html; charset=UTF-8" />'+
-      '</head>'+
-      '<body>'+
-      '<form action="/upload" enctype="multipart/form-data" '+
-      'method="post">'+
-      '<input type="file" name="upload" multiple="multiple">'+
-      '<input type="submit" value="Upload file" />'+
-      '</form>'+
-      '</body>'+
-      '</html>';
-      //'<textarea name="text" rows="20" cols="60"></textarea>'+
-      //'<input type="submit" value="Submit text" />'+
-      //'</form>'+
-      //'</body>'+
-      //'</html>';
-      response.writeHead(200, {"Content-Type": "text/html"});
-      response.write(body);
-      response.end();
+  var body = '<html>'+
+  '<head>'+
+  '<meta http-equiv="Content-Type" '+ 'content="text/html; charset=UTF-8" />'+
+  '</head>'+
+  '<body>'+
+  '<form action="/upload" enctype="multipart/form-data" '+ 
+  'method="post">'+
+  '<input type="file" name="upload" multiple="multiple">'+ 
+  '<input type="submit" value="Upload file" />'+
+  '</form>'+
+  '</body>'+
+  '</html>';
+
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 } 
-  
-//  exec("find /",
-//       { timeout: 10000, maxBuffer: 20000*1024 },
-//       function(error, stdout, stderr) {
-//          response.writeHead(200, {"Content-Type": "text/plain"});
-//          response.write(stdout);
-//          response.end();
-//       });
-//
-
-//  exec('ls -lah', function(error, stdout, stderr) {
-//    response.writeHead(200, {"Content-Type": "text/plain"});
-//    response.write(stdout);
-//    response.end();
-//  });
-//
-
 
 function upload(response, request) {
   console.log('Request handler "upload" was called');
@@ -56,13 +30,14 @@ function upload(response, request) {
     console.log('parsing done');
 
     fs.rename(files.upload.path, "/tmp/test.png", function(error) {
-      if(error) {
-        fs.unlink("/tmp/test.png");
-        fs.rename(files.upload.path, "/tmp/test.png");
-      }
+     if (error) {
+       fs.unlink("/tmp/test.png");
+       fs.rename(files.upload.path, "/tmp/test.png");
+     }
     });
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("img src='/show' />"); 
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
     response.end();
   });
 }
@@ -72,7 +47,6 @@ function show(response) {
   response.writeHead(200, {"Content-Type": "image/png"});
   fs.createReadStream("/tmp/test.png").pipe(response);
 }
-
 
 
 exports.start = start;
